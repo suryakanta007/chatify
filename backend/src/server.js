13a066da72510d712d1350149918d8ab1b786628
suryakanta007 <sahusuryakanta073@gmail.com>
2026-06-07 12:53:21 +0530

    feat: set up server for production deployment and add package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,6 @@
 import http from "http";
+import path from "path";
+import express from "express";
 
 
 import app from "./app.js";
@@ -7,7 +9,20 @@ import connectDB from "./config/db.js";
 
 
 
+
 const server = http.createServer(app);
+const _dirname = path.resolve();
+
+// make ready for deployment
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname,"../frontend/dist")))
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(_dirname,"../frontend/dist/index.html"))
+    })
+}
+
+
 
 await connectDB();
 
